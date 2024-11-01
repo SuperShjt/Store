fdd43c624ce011100c3d29a7292b5be7c0a93820
Marcelino <marconaoum@gmail.com>
2024-11-01 16:04:52 +0200

    Cookie Store

---
+++ cookie-store/src/component/NavSectionOne.jsx
@@ -0,0 +1,21 @@
+
+import logo from '../assets/logo.png'
+function NavSectionOne(){
+
+    return(
+        <>
+        <ul className ="account-shop">
+                <li><a href="#">My Account</a></li>
+                <li><a href="#">Shop</a></li>
+         </ul>
+         <img src= {logo} alt="logo"></img>  
+         </>
+        
+
+
+    )
+
+
+}
+
+export default NavSectionOne
---
+++ cookie-store/src/component/Navbar.jsx
@@ -0,0 +1,20 @@
+
+import SectionOne from "./NavSectionOne"
+
+
+function Navbar(){
+
+    return(
+
+        <navbar>
+            <SectionOne></SectionOne>
+            
+        </navbar>
+
+    
+    )
+
+
+}
+
+export default Navbar
